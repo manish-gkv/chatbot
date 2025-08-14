d36d02437b9ede1a57155bde89cbbbfa57da896c
manish-gkv <manishkumarslmp@gmail.com>
2025-08-14 22:12:29 +0530

    Fixed sending Button

--- src/components/NewChat.jsx
+++ src/components/NewChat.jsx
@@ -9,7 +9,7 @@ export default function NewChat(){
     const [createChat, { loading:creatingChat, error }] = useMutation(CREATE_CHAT);
     const navigate = useNavigate();
     const sendButtonHandler = async () => {
-        if(creatingChat || sendingMessage) return;
+        if(creatingChat ) return;
         if (!message.trim()) {
             toast.error("Message cannot be empty");
             return;
@@ -39,11 +39,12 @@ export default function NewChat(){
                                 value={message}
                                 onChange={(e) => setMessage(e.target.value)}
                             />
-                            <button className="flex justify-end" onClick={sendButtonHandler}
-                                disabled={creatingChat || sendingMessage}>
-                                <IoSend className="text-2xl cursor-pointer hover:text-gray-800 disabled:opacity-50 disabled:cursor-not-allowed" 
-                                />
-                            </button>
+                            <div className="flex justify-end" >
+                                <button className="flex justify-end cursor-pointer disabled:cursor-not-allowed" onClick={sendButtonHandler}
+                                    disabled={creatingChat}>
+                                    <IoSend className={`text-2xl cursor-pointer hover:text-gray-800 `} />
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
